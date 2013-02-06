f68a34fa780ab56d0e370d834e81e192aeced778
Uno <uno@frukt.at>
2013-02-06 18:59:26 +0100

    prepare the add and use background as backup

--- Resources/app/lib/addToCalendar.js
+++ Resources/app/lib/addToCalendar.js
@@ -1,3 +1,6 @@
+/**
+ * This should be renamed to something like "addReminder"
+ */
 function addToCalendar() {
     Ti.API.info("addToCalendar");
     
@@ -6,15 +9,20 @@ function addToCalendar() {
     this.view;
     this.submitBtn;
     
+    var a2c = this;
+
     this.view = Ti.UI.createView({
         backgroundColor: 'black'
     });
     
-    this.submitBtn = Ti.UI.createButton({
+    this.submitBtn10min = Ti.UI.createButton({
         title: "10 min",
         top: 100
     });
-    this.view.add(this.submitBtn);
+    this.submitBtn10min.addEventListener('click', function(e){
+        a2c.addEvent();
+    });
+    this.view.add(this.submitBtn10min);
 }
 
 addToCalendar.prototype.getView = function() {
@@ -22,10 +30,35 @@ addToCalendar.prototype.getView = function() {
 }
 
 /**
- * Do the actual save to calendar
+ * Communicate with the calendar module and save the event
  */
-addToCalendar.prototype.submit = function(when) {
-    Ti.API.info("submit");
+addToCalendar.prototype.addEvent = function(when) {
+    Ti.API.info("addEvent");
+
+    if (platform == 'ios') {
+        Ti.API.info("add to calendar ios");
+    } else {
+        Ti.API.info("add to calendar android");
+    }
+
+    this.view.hide();
 }
 
+/**
+ * Use background service as alarm
+ */
+addToCalendar.prototype.bgAlarm = function(minutes) {
+    Ti.API.info("submit");
+
+    if (platform == 'ios') {
+        alert("Send me to background and I will popup when its time to go!");
+        // If we fail to add to calendar, then add it as a backgroundservice maybe?
+        var service = Ti.App.iOS.registerBackgroundService({
+            url:'bg.js'
+        });
+    } else {
+        Ti.API.info("Att background service andorid");
+    }
+
+}
 module.exports = addToCalendar;
