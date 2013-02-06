8077bf8a051cebc8cc67af64a53836ce02460818
Uno <uno@frukt.at>
2013-02-06 12:16:06 +0100

    cleanup for app in background alarm

--- Resources/bg.js
+++ Resources/bg.js
@@ -1,9 +1,8 @@
 // Not used right now
 var date = new Date();
 var notification = Ti.App.iOS.scheduleLocalNotification({
-    alertBody:"Kitchen Sink was put in background",
+    alertBody:"Its time to go!",
     alertAction:"Re-Launch!",
     userInfo:{"hello":"world"},
-    sound:"pop.caf",
     date:new Date(new Date().getTime() + 3000) // 3 seconds after backgrounding
 });
